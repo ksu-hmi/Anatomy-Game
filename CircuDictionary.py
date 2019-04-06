5e9442d136072e9a3a3b0349cbb1921cb0416eed
ylynch <49033344+ylynch@users.noreply.github.com>
2019-04-06 14:40:59 -0400

    Add files via upload

---
+++ CircuDictionary.py
@@ -0,0 +1,14 @@
+circu_prompts = [
+    "What is the primary responsibility of the circulatory system?\n(a) Oversees the flow of blood, nutrients, hormones, oxygen, and other gases to and from cells\n(b)WRONG ANSWER\n(c)WRONG ANSWER\n(d)WRONG ANSWER\n\n",
+    "True or False:\nThe aorta carries oxygenated blood from the heart to the organs and tissues.\n\n",
+    "Which of the following are part of the heart?\n(a) Superior Vena Cava\n(b) Pulmonary Artery\n(c) Aorta\n(d) All of the above\n\n",
+    "True or False:\nAn average adult has 7-8 quarts of blood.\n\n",
+    "Which independent systems are part of the circulatory system?\n(a) The heart\n(b) The lungs\n(c) The arteries, veins, and coronary and portal vessels\n(d) all of the above\n\n"
+    "True or False:\nOxygen-deprived blood enters into the left ventricle of the heart.\n\n"
+    "What does the aorta do?\n(a) Carries oxygenated blood from the heart to the organs and tissues\n(b) WRONG ANSWER\n(c) WRONG ANSWER\n(d)WRONG ANSWER\n\n"
+    "True or False:\nArteries carry oxygen-rich blood from the heart throughout the body.\n\n"
+    "Which of the following are NOT diseases of the circulatory system?\n(a) Peripheral Arterial Disease\n(b) Arteriosclerosis\n(c)Gastroesophageal Reflux Disease\n(d) Aortic Aneurysm\n\n"
+    "True or False:\nBlood is made up of plasma, red blood cells, white blood cells, and platelets.\n\n"
+]
+
+# need to come up with incorrect answers for the multiple choice questions
