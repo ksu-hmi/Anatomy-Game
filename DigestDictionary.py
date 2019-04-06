75389f18248f02a9bfb3e3359343ab847d3c5a4e
ylynch <49033344+ylynch@users.noreply.github.com>
2019-04-06 15:27:16 -0400

    Add files via upload

---
+++ DigestDictionary.py
@@ -0,0 +1,15 @@
+digest_prompts = [
+    "What is the primary responsibility of the digestive system?\n(a) WRONG ANSWER\n(b) WRONG ANSWER\n(c) WRONG ANSWER\n(d) Converts food into essential nutrients that are absorbed into the body and eliminates unused waste material\n\n",
+    "True or False:\nPeristalsis is a series of muscle contractions that transports food from the esophagus through to the rest of the digestive system.\n\n",
+    "What is NOT part of the digestive system?\n(a) WRONG ANSWER\n(b) Esophagus\n(c) Large Intestine\n(d) Stomach\n\n",
+    "True or False:\nThe stomach's gastric juice consists of hydrochlric acid and pepcid.\n\n",
+    "What does the liver create, from sugars and carbohydrates?\n(a) WRONG ANSWER\n(b) Glycogen\n(c) WRONG ANSWER\n(d) WRONG ANSWER\n\n",
+    "True or False:\nRoutine colonoscopies help in early detection of colon cancer.\n\n",
+    "Which of the following is NOT part of the colon?\n(a) The ascending colon\n(b) The transverse colon\n(c) The rising colon\n(d) The descending colon\n\n",
+    "True or False:\nThe oropharynx and the hypopharynx are responsible for breaking down unwanted chemicals such as alcohol.\n\n",
+    "Which of the following is NOT a disease of the digestive system?\n(a) Irritable Bowel Syndrom\n(b) Diverticulitis\n(c) Chron's Disease\n(d) None of the above\n\n",
+    "True or False:\nChyme, secreted by the salivary gland, is an enzyme that breaks down starch.\n\n"
+]
+
+# need to come up with incorrect answers for the multiple choice questions
+# 1-D, 2-T, 3-A, 4-F, 5-B, 6-T, 7-C, 8-F, 9-D, 10-F"
