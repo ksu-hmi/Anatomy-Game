957ec613484af2a3e459ef4f6d8dd213a71834e7
ylynch <49033344+ylynch@users.noreply.github.com>
2019-04-06 15:25:53 -0400

    Update CircuDictionary.py

--- CircuDictionary.py
+++ CircuDictionary.py
@@ -3,11 +3,11 @@ circu_prompts = [
     "True or False:\nThe aorta carries oxygenated blood from the heart to the organs and tissues.\n\n",
     "Which of the following are part of the heart?\n(a) Diastole\n(b) Fibrinogen\n(c) Superior Vena Cava\n(d) Tunica Media\n\n",
     "True or False:\nAn average adult has 7-8 quarts of blood.\n\n",
-    "Which independent systems are part of the circulatory system?\n(a) The heart\n(b) The lungs\n(c) The arteries, veins, and coronary and portal vessels\n(d) All of the above\n\n"
-    "True or False:\nOxygen-deprived blood enters into the left ventricle of the heart.\n\n"
-    "What does the aorta do?\n(a) WRONG ANSWER\n(b) Carries oxygenated blood from the heart to the organs and tissues\n(c) WRONG ANSWER\n(d) WRONG ANSWER\n\n"
-    "True or False:\nArteries carry oxygen-rich blood from the heart throughout the body.\n\n"
-    "Which of the following are NOT diseases of the circulatory system?\n(a) Peripheral Arterial Disease\n(b) Arteriosclerosis\n(c)Gastroesophageal Reflux Disease\n(d) Aortic Aneurysm\n\n"
+    "Which independent systems are part of the circulatory system?\n(a) The heart\n(b) The lungs\n(c) The arteries, veins, and coronary and portal vessels\n(d) All of the above\n\n",
+    "True or False:\nOxygen-deprived blood enters into the left ventricle of the heart.\n\n",
+    "What does the aorta do?\n(a) WRONG ANSWER\n(b) Carries oxygenated blood from the heart to the organs and tissues\n(c) WRONG ANSWER\n(d) WRONG ANSWER\n\n",
+    "True or False:\nArteries carry oxygen-rich blood from the heart throughout the body.\n\n",
+    "Which of the following are NOT diseases of the circulatory system?\n(a) Peripheral Arterial Disease\n(b) Arteriosclerosis\n(c)Gastroesophageal Reflux Disease\n(d) Aortic Aneurysm\n\n",
     "True or False:\nBlood is made up of plasma, red blood cells, white blood cells, and platelets.\n\n"
 ]
 
