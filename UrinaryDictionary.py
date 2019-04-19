0ab36aa16bdbdeb9887fe30924e689c5d17d9fc1
rachelsykes <46584542+rachelsykes@users.noreply.github.com>
2019-04-18 22:41:32 -0400

    Add files via upload

---
+++ UrinaryDictionary.py
@@ -0,0 +1,16 @@
+
+urinary_prompts = [
+    "The primary organ(s) of the urinary system is/are: \n(a)the liver\n(b )the kidneys\n(c) the pancreas\n(d) the gallbladder\n",
+    "True or False:\nThe urinary system is also known as the renal system.\n",
+    "The kidneys remove urea from the blook through small filtering units called?\n(a) Ureters\n(b) Fibrinogen\n(c) Nephrons\n(d) Tubules\n",
+    "True or False:\nA normal, healthy bladder can hold up to 1 liter of urine comfortably for 2-5 hours\n",
+    "Which of the following are part of the renal system?\n(a) The urethra\n(b) The bladder\n(c) The kidneys\n(d) All of the above\n\n",
+    "True or False:\nSmall amounts of urine are emptied into the bladder from the ureters about every 10-15 seconds.\n",
+    "What does the bladder do?\n(a) WRONG ANSWER\n(b) Stores urine until the brain signals the bladder that the person is ready to empty it.\n(c) WRONG ANSWER\n(d) WRONG ANSWER\n\n",
+    "True or False:\nCircular muscles called sphincters close tightly around the opening of the bladder to prevent leakage into the urethra.\n",
+    "Which of the following are NOT diseases of the urinary system?\n(a) Interstitial cystitis\n(b) Arteriosclerosis\n(c)Kidney Stones\n(d) Urinary tract infections\n\n",
+    "True or False:\nNephrologists treat problems with the urinary tract.\n"
+]
+
+# need to come up with incorrect answers for the multiple choice questions
+# 1-B, 2-T, 3-C, 4-F- (1/2 liter), 5-D, 6-T, 7-B, 8-T, 9-B, 10-F (nephrologists)
