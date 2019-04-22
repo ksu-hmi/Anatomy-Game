2c958957d8eb352a7c381f6fa328d370a5a0b3be
rachelsykes <46584542+rachelsykes@users.noreply.github.com>
2019-04-22 19:59:50 -0400

    Add files via upload

---
+++ respiratory.py
@@ -0,0 +1,23 @@
+question1 = ("What are the primary organs of the respiratory system, responsible for carrying out the exchange of gases as we breathe? ")
+question2 = ("The human respiratory system is a series of organs responsible for taking in oxygen and expelling what other gas? ")
+question3 = ("True or False: Brain cells being dying after about 4 minutes without oxygen. ")
+question4 = ("What is a newborn's normal breathing rate per minute?  ")
+question5 = ("Also known as the windpipe, what filters the air that is inhaled? ")
+question6 = ("True or False: The left lung has 2 lobes, while the right lung has 3.  ")
+question7 = ("Where does the exhange of oxygen and carbon dioxide occur?  ")
+question8 = ("What is the name of the dome-shaped muscle at the bottom of the lungs that controls breathing? ")
+question9 = ("What condition causes coughing, wheezing, and shortness of breath due to inflammation of the lung airways?  ")
+question10 = ("What medical specialists treat conditions of the respiratory system? ")
+
+  
+
+answer1 = ("lungs")
+answer2 = ("carbon dioxide")
+answer3 = ("true")
+answer4 = ("40")
+answer5 = ("trachea")
+answer6 = ("false")
+answer7 = ("alveoli")
+answer8 = ("diaphragm")
+answer9 = ("asthma")
+answer10 = ("pulmonologists")
