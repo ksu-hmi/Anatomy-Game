68f12418b834b5aab91105c62aa9accbfb2e3184
rachelsykes <46584542+rachelsykes@users.noreply.github.com>
2019-04-22 18:29:30 -0400

    Add files via upload

---
+++ muscular.py
@@ -0,0 +1,23 @@
+question1 = ("How many muscles are there in the adult human muscular system? ")
+question2 = ("What are the 3 types of muscles? ")
+question3 = ("Which of the 3 muscle types consist of the only voluntary muscle tissue, controlling every action that a person consciously performs? ")
+question4 = ("Known as involuntary muscle, which muscle type is controlled by the unconscious part of the brain? ")
+question5 = ("Which muscle type is found only in the heart and reponsible for pumping blood throughout the body? ")
+question6 = ("Where in your body is the deltoid muscle situated?  ")
+question7 = ("Where is the smallest skeletal muscle, the stapedius, found in the body?  ")
+question8 = ("What is the largest muscle in the human body? ")
+question9 = ("What is the hardest working muscle, pumping at least 2,500 gallons of blood per day?  ")
+question10 = ("True or False: There is no single type of doctor that treats muscular disesases and disorders. ")
+
+  
+
+answer1 = ("650")
+answer2 = ("skeletal, smooth, and cardiac")
+answer3 = ("skeletal")
+answer4 = ("smooth")
+answer5 = ("cardiac")
+answer6 = ("arm")
+answer7 = ("ear")
+answer8 = ("gluteus maximus")
+answer9 = ("heart")
+answer10 = ("true")
