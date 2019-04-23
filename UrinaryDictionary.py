63dc9e0bb07bf9f66cc66e2960b0c680d851d1fb
rachelsykes <46584542+rachelsykes@users.noreply.github.com>
2019-04-22 21:39:27 -0400

    Update UrinaryDictionary.py

--- UrinaryDictionary.py
+++ UrinaryDictionary.py
@@ -1,16 +1,24 @@
+question1 = ("What are the primary organ(s) of the urinary system? ")
+question2 = ("True or False: The urinary system is also known as the renal system.")
+question3 = ("The kidneys remove urea from the blook through small filtering units called?")
+question4 = ("True or False:\nA normal, healthy bladder can hold up to 1 liter of urine comfortably for 2-5 hours.\n")
+question5 = ("Which of the following are part of the renal system?\n(a) The urethra\n(b) The bladder\n(c) The kidneys\n(d) All of the above\n")
+question6 = ("True or False:\nSmall amounts of urine are emptied into the bladder from the ureters about every 10-15 seconds.\n")
+question7 = ("What does the bladder do?\n(a) Force urine away from the kidneys.\n(b) Stores urine until the brain signals the bladder that the person is ready to empty it.\n(c) Removes urea from the blood.\n(d) Prevents leakage into the urethra.\n")
+question8 = ("True or False:\nCircular muscles called sphincters close tightly around the opening of the bladder to prevent leakage into the urethra.\n")
+question9 = ("Which of the following are NOT diseases of the urinary system?\n(a) Interstitial cystitis\n(b) Arteriosclerosis\n(c)Kidney Stones\n(d) Urinary tract infections\n")
+question10 = ("True or False:\nNephrologists treat problems of urinary tract.\n")
 
-urinary_prompts = [
-    "The primary organ(s) of the urinary system is/are: \n(a)the liver\n(b )the kidneys\n(c) the pancreas\n(d) the gallbladder\n",
-    "True or False:\nThe urinary system is also known as the renal system.\n",
-    "The kidneys remove urea from the blook through small filtering units called?\n(a) Ureters\n(b) Fibrinogen\n(c) Nephrons\n(d) Tubules\n",
-    "True or False:\nA normal, healthy bladder can hold up to 1 liter of urine comfortably for 2-5 hours\n",
-    "Which of the following are part of the renal system?\n(a) The urethra\n(b) The bladder\n(c) The kidneys\n(d) All of the above\n\n",
-    "True or False:\nSmall amounts of urine are emptied into the bladder from the ureters about every 10-15 seconds.\n",
-    "What does the bladder do?\n(a) Force urine away from the kidneys.\n(b) Stores urine until the brain signals the bladder that the person is ready to empty it.\n(c) Removes urea from the blood.\n(d) Prevents leakage into the urethra.\n\n",
-    "True or False:\nCircular muscles called sphincters close tightly around the opening of the bladder to prevent leakage into the urethra.\n",
-    "Which of the following are NOT diseases of the urinary system?\n(a) Interstitial cystitis\n(b) Arteriosclerosis\n(c)Kidney Stones\n(d) Urinary tract infections\n\n",
-    "True or False:\nNephrologists treat problems of urinary tract.\n"
-]
+answer1 = ("kidneys")
+answer2 = ("true")
+answer3 = ("nephrons")
+answer4 = ("false")
+### Answer4: 1/2 liter. ###
+answer5 = ("d")
+answer6 = ("true")
+answer7 = ("b")
+answer8 = ("true")
+answer9 = ("b")
+answer10 = ("false")
 
-# need to come up with incorrect answers for the multiple choice questions
-# 1-B, 2-T, 3-C, 4-F- (1/2 liter), 5-D, 6-T, 7-B, 8-T, 9-B, 10-F (urologists)
+
