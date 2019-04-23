6c7fdd5d0bcd97a583f30952447f4f91e112ec86
rachelsykes <46584542+rachelsykes@users.noreply.github.com>
2019-04-22 21:20:21 -0400

    Add files via upload

---
+++ lymphatic.py
@@ -0,0 +1,24 @@
+question1 = ("True or False: The lymphatic system is a network of tissues and organs that help rid the body of toxins, waste, and other unwanted materials. ")
+question2 = ("What is the name of the clear, colorless fluid transported by the lymphatic system that contains infection-fighting white blood cells? ")
+question3 = ("Located on the left side of the body just above the kidney, what organ acts as a blood filter? ")
+question4 = ("Which white blood cell produced by the spleen acts as defenders against germs?  ")
+question5 = ("What part of the lymphatic system,found in the back of the throat, is one of the immune system's first lines of defense against outside invaders? ")
+question6 = ("What small organ located in the chest just above the heart stores immature lymphocytes and prepares them to become active T cells?  ")
+question7 = ("Which of the following organs is NOT part of the lymphatic system: spleen, thymus, bone marrow, gallbladder?  ")
+question8 = ("True or False: Like blood, lymph flows throughout the body in a continue loop. ")
+question9 = ("Which medical specialists typically treat disesase and disorders of the lymphatic system?  ")
+question10 = ("How many drainage areas does the lymphatic system have in which to clear different parts of the body? ")
+
+  
+
+answer1 = ("true")
+answer2 = ("lymph")
+answer3 = ("spleen")
+answer4 = ("lymphocytes")
+answer5 = ("tonsils")
+answer6 = ("thymus")
+answer7 = ("gallbladder")
+answer8 = ("false")
+### Answer 8:Lymph flows in only one direction-upward toward the neck. ###
+answer9 = ("immunologists")
+answer10 = ("2")
