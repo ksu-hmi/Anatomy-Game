0ab21879d800ea7765469d7d369cc935c60e86cb
ylynch <49033344+ylynch@users.noreply.github.com>
2019-04-27 11:31:10 -0400

    Add files via upload

---
+++ Reproductive.py
@@ -0,0 +1,25 @@
+## reproductive system
+
+question1 = ("When fertilization occurs, sperm must enter through what part of the female reproductive system, in order to burrow into the egg?\n")
+question2 = ("True or False: Bartholin's gland is a part of the female reproductive structure.\n")
+question3 = ("Which part of the male reproductive system is responsible for sperm production?\n")
+question4 = ("True or False: the fetus begins to take shape during a process called embryogenesis.\n")
+question5 = ("In male infertility, what is it called when no sperm cells are produced?\n")
+question6 = ("True or False: cystic fibrosis can cause male infertility.\n")
+question7 = ("What is the term for people with both male and female reproductive parts?\n")
+question8 = ("True or False: the vagina is attached to the uterus through the fallopian tubes.\n")
+question9 = ("Which specialists typically oversee disorders in the male reproductive system?\n")
+question10 = ("True or False: the only way to screen for cervical cancer is through Pap tests/smears.\n")
+
+
+
+answer1 = ("fallopian tube\n")
+answer2 = ("true\n")
+answer3 = ("testes\n")
+answer4 = ("false\n") ## morphogenesis
+answer5 = ("azoospermia\n")
+answer6 = ("true\n")
+answer7 = ("intersex\n")
+answer8 = ("false\n") ## cervix
+answer9 = ("urologists\n")
+answer10 = ("false\n") ## pap tests/smears and HPV tests
